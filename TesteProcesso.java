95d56c221544bc64f8d017caa9116af5b48ffc74
Victor Ponciano <victordias.ponciano@gmail.com>
2026-04-06 15:49:07 -0300

    Complemento da Etapa 1

---
+++ TesteProcesso.java
@@ -0,0 +1,12 @@
+public class TesteProcesso {
+    
+    public static void main(String[] args) {
+
+        Processo p1 = new Processo("Victor", "Financeiro", 3);
+        // FELIPE EH O NOME DO MEU IRMAO GEMEO, EH UMA HOMENAGEM
+        Processo p2 = new Processo("Felipe", "Saude", 1);
+
+        System.out.println(p1);
+        System.out.println(p2);
+    }
+}
